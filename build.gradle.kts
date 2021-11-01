3976a2e0929d2b5aeae77a6d8c44dadf889156b8
Simao Ferreira <24299428+simao-ferreira@users.noreply.github.com>
2021-11-01 21:33:20 +0100

    TEMPLATE: Update build gradle file

--- build.gradle.kts
+++ build.gradle.kts
@@ -12,12 +12,12 @@ repositories {
 }
 
 dependencies {
-    implementation(kotlin("stdlib"))
-    testImplementation("org.junit.jupiter:junit-jupiter-api:5.6.0")
-    testImplementation( "org.codehaus.groovy:groovy-all:3.0.5")
-    testImplementation( "org.spockframework:spock-core:2.0-M3-groovy-3.0")
-    testImplementation( "org.spockframework:spock-spring:2.0-M3-groovy-3.0")
-    testRuntimeOnly("org.junit.jupiter:junit-jupiter-engine")
+    implementation("org.jetbrains.kotlin:kotlin-stdlib:1.5.31")
+    testImplementation("org.junit.jupiter:junit-jupiter-api:5.8.1")
+    testImplementation("org.codehaus.groovy:groovy-all:3.0.8")
+    testImplementation("org.spockframework:spock-core:2.0-groovy-3.0")
+    testImplementation("org.spockframework:spock-spring:2.0-groovy-3.0")
+    testRuntimeOnly("org.junit.jupiter:junit-jupiter-engine:5.8.1")
 }
 
 tasks.getByName<Test>("test") {
--- src/main/kotlin/Main.kt
+++ src/main/kotlin/Main.kt
@@ -1,5 +1,5 @@
 class Main
 
-fun main(args: Array<String>) {
+fun main() {
     println("Hello world!")
 }
